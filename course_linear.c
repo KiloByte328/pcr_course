4a49c4a0506d9da328c63eea1920630bc511bdb8
KiloByte328 <56058011+KiloByte328@users.noreply.github.com>
2024-12-17 13:48:46 +0700

    update linnear version

--- course_linear.c
+++ course_linear.c
@@ -12,34 +12,30 @@ int main(int argc, char** argv)
     {
         for (size_t x = 0; x < m; x++)
         {
-            Matr[y*m + x] = rand() % (RAND_MAX - 100);
+            Matr[y * m + x] = rand() % 100;
         }
     }
     double now = MPI_Wtime();
     double Div;
     for (size_t grid_walker = 0; grid_walker < n; grid_walker++)
     {
+        Div = Matr[grid_walker * m + grid_walker];
+       /*  printf("checking the prelast and last numbers in grid: %f %f\nDiv is: %f\n", Matr[(n-1) * (m-2)], Div);     */
         if (Matr[grid_walker * m + grid_walker] == 0.0)
             return 1;
         if (Matr[grid_walker * m + grid_walker] != 1) {
-            for (size_t x = 0; x < m; x++)
+            for (size_t x = grid_walker; x < m; x++)
             {
-                x == grid_walker ? x++ : x;
-                Matr[grid_walker * m + x] = Matr[grid_walker * m + x] / Matr[grid_walker * m + grid_walker];
+                Matr[grid_walker * m + x] = Matr[grid_walker * m + grid_walker] >= 1 ? (Matr[grid_walker * m + x] / Div) : (Matr[grid_walker * m + x] * (1 / Div));
             }
-            Matr[grid_walker * m + grid_walker] >= 1 ? (Matr[grid_walker * m + grid_walker] = Matr[grid_walker * m + grid_walker] / Matr[grid_walker * m + grid_walker]) :
-            (Matr[grid_walker * m + grid_walker] = Matr[grid_walker * m + grid_walker] * (1 / Matr[grid_walker * m + grid_walker]));
         }
-        for (size_t y = 0; y < n; y++)
+        for (size_t y = grid_walker + 1; y < n; y++)
         {
             y + 1 != n ? (y == grid_walker ? y++ : y) : y;
-            Div = Matr[y * m + grid_walker] / Matr[grid_walker * m + grid_walker];
+            Div = Matr[y * m + grid_walker] >= 1 ? Matr[y * m + grid_walker] / Matr[grid_walker * m + grid_walker] : Matr[y * m + grid_walker] * (1 / Matr[grid_walker * m + grid_walker]);
             for(size_t x = grid_walker; x < m; x++)
             {
-                if (x == grid_walker)
-                    Matr[y * m + x] = Matr[y * m + x] - (Div * Matr[grid_walker * m + grid_walker]);
-                else
-                    Matr[y * m + x] = Matr[y * m + x] - (Div * Matr[grid_walker * m + x]);
+                Matr[y * m + x] = Matr[y * m + x] - (Div * Matr[grid_walker * m + x]);
             }
         }
     }
@@ -47,20 +43,17 @@ int main(int argc, char** argv)
     {
         for (size_t y = grid_back - 1; y > 0; y--) 
         {
-            Div = Matr[(grid_back - 1) * m + grid_back] / Matr[grid_back * m + grid_back];
-            for (size_t x = m - 1; x >= grid_back; x--)
+            Div = Matr[y * m + grid_back] * Matr[grid_back * m + grid_back];
+           for (size_t x = m-1; x >= grid_back; x--)
             {
-                Matr[y * m + x] = Matr[y * m + x] - Matr[grid_back * m + x] * Div;
+                Matr[y * m + x] = Matr[y * m + x] - (Matr[grid_back * m + x] * Div);
             }
         }
     }
     double end = MPI_Wtime() - now;
-    for (size_t y = 0; y < 1; y++)
+    for (size_t y = 0; y != 2000; y++)
     {
-        for (size_t x = 0; x < m; x++)
-        {
-            printf ("y = %d, x = %d: %f \n", y, x, Matr[y * m + x]);
-        }
+        printf ("y = %d, x = %d: %f, sum: %f\n", y, y, Matr[y*m + y], Matr[y * m + m-1]);
     }
     printf("Time of work is: %f \n", end);
     return 0;
