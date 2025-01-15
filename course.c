bda3dab2e329408f3fac0a746769443ada5732dd
KiloByte328 <56058011+KiloByte328@users.noreply.github.com>
2025-01-15 19:15:49 +0700

    Update course.c

--- course.c
+++ course.c
@@ -12,8 +12,8 @@ int main(int argc, char** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &commsize);
   srand(rank);
-  int n = 1000;
-  int m = n + 10;
+  int n = 3000;
+  int m = n * 2;
   int my_begin = rank == 0 ? 0 : rank * (n / commsize);
   int my_end = rank == commsize - 1 ? n : (rank + 1) * (n / commsize);
   int my_part = my_end - my_begin;
@@ -60,43 +60,43 @@ int main(int argc, char** argv) {
                      ? commsize - 1
                      : get_rnk(commsize, n, grid_walker)),
                 MPI_COMM_WORLD);
-      rank == commsize - 1 ? printf("i want to modyfi: %f, i get this to modyfi: %f\n", Matr[grid_walker % my_part + grid_walker], Others[grid_walker]) : 0;
-      if (my_end > grid_walker)
+      if (grid_walker < my_end)
         for (size_t y = 0; y < my_part; y++) {
-          Div = Matr[y * m + grid_walker] >= 1 ? Matr[y * m + grid_walker] / Others[grid_walker] : Matr[y * m + grid_walker] * ( 1 / Others[grid_walker]);
+          Div = Matr[y * m + grid_walker] >= 1
+                                ? Matr[y * m + grid_walker] / Others[grid_walker] 
+                                : Matr[y * m + grid_walker] * (1 / Others[grid_walker]);
           for (size_t x = grid_walker; x < m; x++) {
             Matr[y * m + x] = Matr[y * m + x] - (Div * Others[x]);
           }
         }
     }
-    rank == commsize - 1 ? printf("i modyfied it: %f, grid_walker is: %d\n", Matr[grid_walker % my_part + grid_walker], grid_walker) : 0;
   }
   for (int grid_back = n - 1; grid_back >= 1; grid_back--) {
-    if (grid_back > my_begin && grid_back < my_end) {
+    if (grid_back >= my_begin && grid_back < my_end) {
+      int ehh = rank == commsize - 1 ? n - grid_back: (rank + 1) * (n / commsize) - grid_back; 
       if (grid_back == my_begin) {
         int for_y = 1;
         Div = Matr[for_y * m + grid_back] * Matr[(for_y - 1) * m + grid_back];
-        for (int x = my_part; x >= grid_back; x--) {
-          Matr[(grid_back - 1) * m + x] -= Matr[for_y * m + x] * Div;
+        for (int x = m - 1; x >= grid_back; x--) {
+          Matr[(for_y - 1) * m + x] -= Matr[for_y * m + x] * Div;
         }
         MPI_Bcast(&Matr[for_y], 1, row, rank, MPI_COMM_WORLD);
-        for (int y = (for_y - 1); y >= 0; y--) {
+        for (int y = for_y - 1; y >= 0; y--) {
           Div = Matr[y * m + grid_back] * Matr[for_y * m + grid_back];
           for (int x = m - 1; x >= grid_back; x--) {
             Matr[y * m + x] = Matr[y * m + x] - (Matr[y * m + x] * Div);
           }
         }
       } else {
-        Div = Matr[grid_back % my_part * m + grid_back] *
-              Matr[(grid_back % my_part - 1) * m + grid_back];
-        for (int x = my_part; x >= grid_back; x--) {
-          Matr[(grid_back % my_part - 1) * m + x] -=
-              Matr[grid_back % my_part * m + x] * Div;
+        Div = Matr[(my_part - ehh) * m + grid_back] *
+              Matr[((my_part - ehh) - 1) * m + grid_back];
+        for (int x = m - 1; x >= grid_back; x--) {
+          Matr[((my_part - ehh) - 1) * m + x] -=
+              Matr[(my_part - ehh) * m + x] * Div;
         }
-        MPI_Bcast(&Matr[grid_back % my_part], 1, row, rank, MPI_COMM_WORLD);
-        for (int y = (grid_back - 1) % my_part; y >= 0; y--) {
-          Div = Matr[y * m + grid_back] *
-                Matr[grid_back % my_part * m + grid_back];
+        MPI_Bcast(&Matr[(my_part - ehh) * m], 1, row, rank, MPI_COMM_WORLD);
+        for (int y = (my_part - ehh) - 1; y >= 0; y--) {
+          Div = Matr[y * m + grid_back] * Matr[(my_part - ehh) * m + grid_back];
           for (int x = m - 1; x >= grid_back; x--) {
             Matr[y * m + x] = Matr[y * m + x] - (Matr[y * m + x] * Div);
           }
@@ -108,8 +108,7 @@ int main(int argc, char** argv) {
           (grid_back >= n - (n % commsize) ? commsize - 1
                                            : get_rnk(commsize, n, grid_back)),
           MPI_COMM_WORLD);
-      rank == commsize - 1 ? printf("grid is: %d, i get: %f, i want modyfi: %f\n", grid_back, Others[grid_back], Matr[grid_back % my_part * m + grid_back]): 0;
-      if (my_begin < grid_back)
+      if (my_end <= grid_back)
         for (int y = (grid_back - 1) % my_part; y >= 0; y--) {
           Div = Matr[y * m + grid_back] * Others[grid_back];
           for (int x = m - 1; x >= grid_back; x--) {
@@ -118,21 +117,18 @@ int main(int argc, char** argv) {
         } 
     }
   }
-  //printf("my part is: %d, my start is: %d, my end is: %d \n", my_part, my_begin, my_end);
   for(int y = 0; y < n; y++)
   {
 	  if (y >= my_begin && y < my_end)
-		MPI_Bcast(&Matr[y % my_part * n + n - 1], m - n - 1, MPI_DOUBLE, rank, MPI_COMM_WORLD);
+		MPI_Bcast(&Matr[y % my_part * n + n], n, MPI_DOUBLE, rank, MPI_COMM_WORLD);
 	  else
-		MPI_Bcast(&Res[y * (m - n)],  m - n - 1, MPI_DOUBLE, (y >= n - n % commsize ? commsize - 1: get_rnk(commsize, n, y)), MPI_COMM_WORLD);
-	  // поставь сюда дома Allreduce вместо бкастов
+		MPI_Bcast(&Res[y * (m - n)],  n, MPI_DOUBLE, (y >= n - n % commsize ? commsize - 1: get_rnk(commsize, n, y)), MPI_COMM_WORLD);
   }
   for (int y = 0; y < my_part; y++)
   {
 	for(int x = 0; x < m - n; x++)
 	{
-		Res[my_begin + y + x] = Matr[y * m + x];
-		rank == 1 ? printf("Copy form y = %d, x = %d, what copied: %f\n", y, x, Matr[y * m + x]) : 0;
+		Res[my_begin + (y * n) + x] = Matr[y * m + n + x];
 	}
   }
   MPI_Barrier(MPI_COMM_WORLD);
